ec09a9848988545357da34fd145b4c65abae80c4
stifskere <stifskere@gmail.com>
2024-09-29 09:45:24 +0200

    chore: refactored error system and abstracted functions into own file

--- src/bandb/bits.rs
+++ src/bandb/bits.rs
@@ -1,24 +1,48 @@
+use std::{num::TryFromIntError, str::FromStr};
+
+use thiserror::Error;
+
 pub struct Bits {
-    pub value: Vec<u8>,
+    value: Vec<u8>,
+}
+
+#[derive(Debug, Error)]
+pub enum BitsConversionError {
+    #[error("Cannot convert byte to digit")]
+    ConversionError,
+
+    #[error("Cannot downcast value to u8")]
+    CastError(#[from] TryFromIntError),
 }
 
 impl Bits {
-    pub fn from_vec(bits: Vec<u8>) -> Self {
-        Bits { value: bits }
+    pub fn from_iter(bits: &[u8]) -> Self {
+        Bits { value: bits.into() }
     }
 
-    pub fn from_str(str: &str) -> Self {
-        let mut vec: Vec<u8> = vec![];
-        for b in str.bytes() {
+    pub fn value(&self) -> &Vec<u8> {
+        &self.value
+    }
+}
+
+impl FromStr for Bits {
+    type Err = BitsConversionError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let mut vec = Vec::new();
+
+        for b in s.bytes() {
             vec.push(
                 u8::try_from(
                     (b as char)
                         .to_digit(10)
-                        .expect("Cannot convert vec to digit"),
-                )
-                .expect("Cannot cast u32 to u8"),
+                        .ok_or(BitsConversionError::ConversionError)?
+                )?,
             );
         }
-        Bits { value: vec }
+
+        Ok(Self {
+            value: vec
+        })
     }
 }
--- src/bandb/byte.rs
+++ src/bandb/byte.rs
@@ -1,33 +1,68 @@
+use std::{num::{ParseIntError, TryFromIntError}, str::FromStr};
+use thiserror::Error;
+use super::bits::Bits;
+
 pub struct Byte {
-    pub value: u8,
+    value: u8,
 }
 
-use crate::bits::Bits;
+#[derive(Debug, Error)]
+pub enum ByteConversionError {
+    #[error("Cannot convert byte to digit")]
+    Conversion,
+
+    #[error("Cannot downcast value to u8")]
+    Cast(#[from] TryFromIntError),
+
+    #[error("{0:?}")]
+    Creation(#[from] ByteCreationError)
+}
+
+#[derive(Debug, Error)]
+pub enum ByteCreationError {
+    #[error("Couldn't parse byte value")]
+    ParseError(#[from] ParseIntError)
+}
 
 impl Byte {
-    pub fn from_bits(bits: &Bits) -> Self {
-        let byte = convert_vec_to_u8(&bits.value);
-        Byte { value: byte }
+    pub fn from_bits(bits: &Bits) -> Result<Self, ByteCreationError> {
+        Self::from_iter(bits.value())
     }
 
-    pub fn from_str(str: &str) -> Self {
-        let mut vec: Vec<u8> = vec![];
-        for b in str.bytes() {
-            vec.push(
-                u8::try_from(
-                    (b as char)
-                        .to_digit(10)
-                        .expect("Cannot convert vec to digit"),
-                )
-                .expect("Cannot cast u32 to u8"),
-            );
-        }
-        let byte = convert_vec_to_u8(&vec);
-        Byte { value: byte }
+    pub fn from_iter(iter: &[u8]) -> Result<Self, ByteCreationError> {
+        Ok(Self {
+            value: u8::from_str_radix(
+                iter
+                    .iter()
+                    .map(|&byte| byte.to_string())
+                    .collect::<String>()
+                    .as_str(),
+                2
+            )?
+        })
+    }
+
+    pub fn value(&self) -> &u8 {
+        &self.value
     }
 }
 
-fn convert_vec_to_u8(vec: &[u8]) -> u8 {
-    let binary_chain: String = vec.iter().map(|&b| b.to_string()).collect();
-    u8::from_str_radix(&binary_chain, 2).expect("Invalid binary slice")
+impl FromStr for Byte {
+    type Err = ByteConversionError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let mut vec = Vec::new();
+
+        for byte in s.bytes() {
+            vec.push(
+                u8::try_from(
+                    (byte as char)
+                        .to_digit(10)
+                        .ok_or(ByteConversionError::Conversion)?
+                )?
+            )
+        }
+
+        Ok(Self::from_iter(&vec)?)
+    }
 }
